bdc00dbad03f615ec5c7ba6cd3a10e04a08f0796
deden2904 <39203619+deden2904@users.noreply.github.com>
2022-06-23 15:49:04 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'd0f68eef95', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
